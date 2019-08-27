a8403ac6030207eaa9fa0beec84f3c19ebd17029
Gus <gus33000@live.fr>
2019-08-27 17:55:21 +0200

    Install GripProxy during oobe

--- Support/Setup/IHVDriversOobe.cmd
+++ Support/Setup/IHVDriversOobe.cmd
@@ -1,2 +1,3 @@
 @echo off
 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\NXPPN547.inf ACPI\PN547
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\proxy_driver.inf') do \Windows\OEM\devcon.exe install %%f Root\GripDet
